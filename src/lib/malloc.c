4ade44e2badddb123ffdc51dac376a9e5e8d8b84
tristansinclair <tristans@stanford.edu>
2020-10-20 13:39:31 -0700

    working basic malloc, free and heap dump
    need to implement with realloc

--- src/lib/malloc.c
+++ src/lib/malloc.c
@@ -22,7 +22,7 @@
 extern int __bss_end__;
 
 #define STACK_START 0x8000000
-#define STACK_SIZE  0x1000000
+#define STACK_SIZE 0x1000000
 #define STACK_END ((char *)STACK_START - STACK_SIZE)
 
 /*
@@ -44,57 +44,121 @@ static void *heap_end = &__bss_end__;
 void *sbrk(int nbytes)
 {
     void *prev_end = heap_end;
-    if ((char *)prev_end + nbytes > STACK_END) {
+    if ((char *)prev_end + nbytes > STACK_END)
+    {
         return NULL;
-    } else {
+    }
+    else
+    {
         heap_end = (char *)prev_end + nbytes;
         return prev_end;
     }
 }
 
-
 // Simple macro to round up x to multiple of n.
 // The efficient but tricky bitwise approach it uses
 // works only if n is a power of two -- why?
-#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+#define roundup(x, n) (((x) + ((n)-1)) & (~((n)-1)))
 
-void *malloc (size_t nbytes)
+struct header
 {
-    // TODO: replace with your code
-    nbytes = roundup(nbytes, 8);
-    return sbrk(nbytes);
+    unsigned int size;   // 4 bytes
+    unsigned int status; // 4 bytes
+} header;
+enum
+{
+    IN_USE = 0,
+    FREE = 1
+};
+
+void *malloc(size_t nbytes)
+{
+    if (nbytes == 0)
+    {
+        return NULL;
+    }
+
+    nbytes = roundup(nbytes, 8);     // align on 8 byte system
+    size_t total_bytes = nbytes + 8; // total bytes needed to be allocated b/c header
+
+    void *prev_end = sbrk(total_bytes); // points to the last byte of the prev block
+    if ((int *)(prev_end) == NULL)       // if there isn't room for your request (heap is full)
+    {
+        return NULL;
+    }
+
+    struct header *hdr = (struct header *)((char *)prev_end + 1); // hdr points to start point of malloc
+    hdr->size = nbytes;                                           // stores bytes requested WITHOUT HEADER SIZE
+    hdr->status = IN_USE;
+
+    void *data_start = (void *)(hdr + 1); // (hdr + 1) is our data start, then cast to a void ptr
+
+    return data_start;
 }
 
-void free (void *ptr)
+void free(void *ptr)
 {
-    // TODO: fill in your own code here
+    struct header *hdr = ptr;
+    hdr--; // moves back 8 bytes to the beg of the header
+    if (hdr->status == IN_USE)
+    {
+        hdr->status = FREE;
+    }
 }
 
-void *realloc (void *orig_ptr, size_t new_size)
+// static void *get_free_block(size_t nbytes)
+// {
+//     int *search = heap_start;
+//     int *size = search;
+//     int *status = search++;
+
+//     while (1)
+//         if (*status == 1 && nbytes < *size)
+//         {
+//             return search;
+//         }
+// }
+
+void *realloc(void *orig_ptr, size_t new_size)
 {
-    // TODO: replace with your code
+
     void *new_ptr = malloc(new_size);
-    if (!new_ptr) return NULL;
+    if (!new_ptr)
+        return NULL;
+
     // ideally would copy the min of new_size and old_size, but this allocator
-    // doesn't know the old_size. Why not? 
+    // doesn't know the old_size. Why not?
     // Why is it "safe" (but not efficient) to copy new_size bytes?
     memcpy(new_ptr, orig_ptr, new_size);
     free(orig_ptr);
     return new_ptr;
 }
 
-void heap_dump (const char *label)
+void heap_dump(const char *label)
 {
     printf("\n---------- HEAP DUMP (%s) ----------\n", label);
     printf("Heap segment at %p - %p\n", heap_start, heap_end);
-    // TODO: fill in your own code here
+
+    struct header *hdr = (struct header *)(heap_start);
+    int size = hdr->size;
+    int status = hdr->status;
+
+    while ((void *)hdr < heap_end)
+    {
+        printf("Header Location: %p     Block Location: %p     Size: %d     Status: %d\n", hdr, hdr + 1, size, status);
+
+        hdr += 1 + (size / 8); // +1 to move to data, +(size/8) to move to next header
+        size = hdr->size;
+        status = hdr->status;
+    }
+
     printf("----------  END DUMP (%s) ----------\n", label);
 }
 
-void memory_report (void)
+void memory_report(void)
 {
     printf("\n=============================================\n");
-    printf(  "         Mini-Valgrind Memory Report         \n");
-    printf(  "=============================================\n");
+    printf("         Mini-Valgrind Memory Report         \n");
+    printf("=============================================\n");
     // TODO: fill in for extension
 }
